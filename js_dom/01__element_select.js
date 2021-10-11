8ed0b8833fccc17c5333205e5c0d6c30ad156ba1
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-10-11 09:39:16 +0600

    codes updated

---
+++ js_dom/01__element_select.js
@@ -0,0 +1,28 @@
+//  'document' keyword is very important in DOM. Because every items are included inside this and if we select any item first we need to writh 'document' keyword.
+
+//  1. Select By ID -- if we look at our html document, every boxes are selected by 'id' like box1, box2, box3, box4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
