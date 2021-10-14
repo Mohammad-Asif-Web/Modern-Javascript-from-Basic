74abf6a6bc70af233bace219ccc5ade5ca0c300c
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-10-14 10:48:22 +0600

    new files created

---
+++ js_dom/02__playWithDom_part1.js
@@ -0,0 +1,53 @@
+        //  DOM MANIPULATION
+        //  Properties & Methods
+
+// append()             // parentElement  
+// appendChild()        // prepend()
+// Children             // previousSibling
+// classList            // remove()
+// CreateElement        // removeChild()
+// getAtrribute()       // setAttribute()
+// innerText            // style
+// innerHTML            // textColor
+// nextSibling          // value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
