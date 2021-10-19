0963822b1d1cb40f9d281c0919808f00e963990a
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-10-19 10:54:04 +0600

    codes changes dom2

--- js_dom/02__playWithDom_part1.js
+++ js_dom/02__playWithDom_part1.js
@@ -1,50 +1,90 @@
         //  DOM MANIPULATION
         //  Properties & Methods
 
-// append()             // parentElement  
-// appendChild()        // prepend()
-// Children             // previousSibling
-// classList            // remove()
-// CreateElement        // removeChild()
-// getAtrribute()       // setAttribute()
-// innerText            // style
-// innerHTML            // textColor
-// nextSibling          // value
+// parentElement 
+// Children
+// classList
+// getAtrribute()
+// innerText
+// innerHTML
+// nextSibling
+// previousSibling
+// setAttribute()
+// style
+// textColor
+
+
+                        //createElement()
+const newElement = document.createElement('li');
+// now we can see, which methods and properties we can implement into this 'li'. 
+console.dir(newElement);
+// adding text to 'li' element
+newElement.innerText = 'Item Five'
+console.log(newElement);
+
+// now selecting an Element from index.
+const ul = document.querySelector('#box4 ul');
+console.log(ul);
+
+                        //append and append child
+// append means add the element to last.
+// appentchild takes only one element, but append takes multiple element like appent(newElement, other, another, etc);
+// we will add the newElement('li') at last into box4 element.
+ul.appendChild(newElement)
+
+                        //prepend
+
+
+
+
+
+
+// insertBefore()
+// insertAdjacentElement()
+// prepend()
+// removeChild()
+// remove()
+// value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //      javascript color text design
 console.log('%cThis is javascript color text', "color: green");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
