37285dcdcd09ec59a5ade34c4377f3882af497b0
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-10-26 13:17:20 +0600

    add,remove animation & audio play by click event

--- Task/add_remove_anime.js
+++ Task/add_remove_anime.js
@@ -1,66 +1,80 @@
 const body = document.body;
-const section = document.createElement('section');
-const div1 = document.createElement('button');
-const div2 = document.createElement('button');
-const div3 = document.createElement('button');
+const section = document.createElement("section");
+const div1 = document.createElement("button");
+const div2 = document.createElement("button");
+const div3 = document.createElement("button");
 
 body.prepend(section);
 
 let sectionStyle = {
-    width: '100%',
-    height: '150px',
-    background: 'firebrick',
-    textAlign: 'center',
-    margin: '0 auto',
-    display: 'flex',
-    justifyContent: 'space-evenly'
-}
-Object.assign(section.style, sectionStyle)
+  width: "100%",
+  height: "150px",
+  background: "firebrick",
+  textAlign: "center",
+  margin: "0 auto",
+  display: "flex",
+  justifyContent: "space-evenly",
+};
+Object.assign(section.style, sectionStyle);
 
 section.prepend(div1);
 section.prepend(div2);
 section.prepend(div3);
-div1.innerText = 'a';
-div2.innerText = 'b';
-div3.innerText = 'c';
+div1.innerText = "a";
+div2.innerText = "b";
+div3.innerText = "c";
 
 let divStyle = {
-    width : '150px',
-    height: '100px',
-    background: 'salmon',
-    color: '#fff',
-    textTransform: 'uppercase',
-    fontWeight: '700',
-    fontSize: '2rem',
-    margin: '25px',
-    border: 'none'
-}
+  width: "150px",
+  height: "100px",
+  background: "salmon",
+  color: "#fff",
+  textTransform: "uppercase",
+  fontWeight: "700",
+  fontSize: "2rem",
+  margin: "25px",
+  border: "none",
+};
 Object.assign(div1.style, divStyle);
 Object.assign(div2.style, divStyle);
 Object.assign(div3.style, divStyle);
 
-const button = document.getElementsByTagName('button');
-const len = document.getElementsByTagName('button').length;
+const button = document.getElementsByTagName("button");
+const len = document.getElementsByTagName("button").length;
 const arr = Array.from(button);
 
-for(let i = 0; i < len; i++){
-    arr[i].addEventListener('click', function(){
-    arr[i].classList.add('anim');
-    setTimeout(function(){
-        arr[i].classList.remove('anim');
+for (let i = 0; i < len; i++) {
+  arr[i].addEventListener("click", function () {
+    // add/remove anime code
+    arr[i].classList.add("anim");
+    setTimeout(function () {
+      arr[i].classList.remove("anim");
     }, 1000);
 
-    var text = this.innerText
-
-    switch(text){
-        case 'a':
-            var audio = new Audio('');
-        
-            audio.play();
-            break;
-    }
-})
+    // play audio js code
+    var text = this.innerText;
+    audioPlay(text);
+  });
 }
+
+function audioPlay(text) {
+    switch (text) {
+      case "A":
+        var audio = new Audio("../assets/a.mp3");
+        audio.play();
+        // audio.pause();
+        break;
+      case "B":
+        var audio = new Audio("../assets/c.mp3");
+        audio.play();
+        break;
+      case "C":
+        var audio = new Audio("../assets/bb.mp3");
+        audio.play();
+        break;
+    }
+  }
+
 
 // div1.addEventListener('click', function(){
 //     div1.classList.add('anim');
@@ -82,6 +96,4 @@ for(let i = 0; i < len; i++){
 //         div3.classList.remove('anim');
 //     }, 1000);
 // })
-
-
 
