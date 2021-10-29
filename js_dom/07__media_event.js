d873d159b1d33ee6d5c1fc59f0f9bd22f8f818fe
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-10-30 00:11:24 +0600

    source, muted, controls attributes and canplay, play, playing, pause, ended, volumechange, waiting, progress events

--- js_dom/07__media_event.js
+++ js_dom/07__media_event.js
@@ -6,17 +6,39 @@ body.prepend(video);
 video.prepend(source);
 video.setAttribute('width', '400');
 video.setAttribute('controls',"");
-video.setAttribute('loop', '');
-video.setAttribute('muted', '')
+// video.setAttribute('loop', '');
+video.setAttribute('muted', "")
 source.setAttribute('src', '../assets/jonas_blue.mp4')
 source.setAttribute('type', 'video/mp4')
 source.innerText = 'Your browser does not support html5 video element'
 
-video.addEventListener('canplay', videoHandler);
+video.addEventListener('canplay', function(){
+    console.log('canplay'); //when the video is ready to play and everything is ok to play directly this event will occur.
+})
+video.addEventListener('play', function(){
+    console.log('play');    //this event occurs when play button is clicked
+});
+video.addEventListener('playing', function(){
+    console.log('playing'); // this event occurs when the video is playing
+})
+video.addEventListener('pause', function(){
+    console.log('pause button clicked');    // when the play button is paused, then it happens
+});
 
-function videoHandler(event){
-    console.log('canplay');
-}
+video.addEventListener('ended', function(){
+    console.log('thanks for watching'); //this event occurs when video is properly ended. But if the loop is on, this event won't work.
+})
+
+video.addEventListener('volumechange', function(){
+    console.log('volume is changing');
+})
+
+// waiting event happens when video is stopped for browser buffering
+// progress event happen when is able to play in progress for buffer.
+
+
+
+
 
 
 
