1243439966b74d52a638ce676ea5643ad4013aa1
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-12-26 02:07:26 +0600

    json files practice

---
+++ JSON/access_modify_delete.js
@@ -0,0 +1,23 @@
+const data = require("./simpleText.json");
+const stData = require("./students.json");
+console.log(data);
+
+// data access from json
+// simpleText file
+const dataOne = data.friends[0].name
+console.log(dataOne);
+const dataTwo = data.friends[1].age
+console.log(dataTwo);
+// student file
+const stdOne = stData.student[0].languages
+const stdTwo = stData.student[0].friends[0].name
+console.log(`languages = ${stdOne}`);
+console.log(`name : ${stdTwo}`);
+
+// data modify from json
+// const modOne = data.friends[1].name = "sergio ramos"
+// console.log(modOne);
+// console.log(data);
+
+
+
