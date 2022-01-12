864237e44b28dd0a5fad36589c68aa72ca6c3015
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2022-01-12 19:09:53 +0600

    local json created

--- javascript_api/04__calling_local_json_data.js
+++ javascript_api/04__calling_local_json_data.js
@@ -1,24 +1,40 @@
-console.clear();
+console.clear
+const body = document.body
+const h1 = document.createElement('h1');
+const button = document.createElement('button');
+const p = document.createElement('p');
+body.prepend(button, h1, p);
+h1.innerText = 'Local JSON Data'
+button.innerText = 'Get data';
 
-
-// const local = require("./local.json");
+button.addEventListener('click', fetchData)
 
 function fetchData(){
     const xml = new XMLHttpRequest();
-    xml.open('GET', 'local.json', true)
+    xml.open('GET', '../JSON/locl.json', true)
 
     xml.onload = function(){
         if(this.status == 200){
-            const data = JSON.parse(this.responseText);
-            console.log(data);
+            try {
+                const data = this.responseText;
+                p.innerText = data
+                console.log(this.responseText);
+            } catch (e){
+                console.warn(`An Error Occured`);
+            }
         }
-        // if(this.status == 404){
-        //     console.log(`404: Not Found`);
-        // }
+        if(this.status == 404){
+            const error = `404: Not Found`
+            p.innerText = error
+            console.log(error);
+        }
+    
+    }
+    xml.onerror = function(){
+        console.log(`error is here`);
     }
     xml.send();
 }
 
-fetchData();
 
 
